3f682579d41b6d5e8049b6b6e3b0922e84aa95e8
idilismail <idil.ismail@warwick.ac.uk>
2022-05-06 10:40:45 +0100

    edited the spectator file

--- KPM/scripts/spectator.py
+++ KPM/scripts/spectator.py
@@ -1,32 +1,33 @@
 # Routine to calculate connectivity matrix of ASE object...
+import numpy as np
 import ase
 from ase.io import read
-import numpy as np
-from ase import Atoms
-from NewModules import*
+import glob
+
+
 def GetGraph(cstr):
     """
     This subroutine takes an ASE object called cstr and calculates the connectivity (adjacency) matrix using the
     same definitions as used in the original CDE code.
     """
-
+    
     # Get the list of atom symbols
     atsym = cstr.get_chemical_symbols()
-
+    
     # Set number of atoms
     n = len(atsym)
-
+    
     # Get positions
     positions = cstr.get_positions()
-
+    
     # Zero-out the graph.
     graph = np.zeros([n,n],dtype='int')
-
+    
     # Set up a dictionary of covalent radii....in Angstroms.
     # NOTE: You'll need to add elements if they're not here!!!
-    CovalentRadii = { 'H' : 0.31,
+    CovalentRadii = { 'H' : 0.31, 
                   'C' : 0.76,
-                  'N' : 0.71,
+                  'N' : 0.71, 
                   'O' : 0.66
                 }
     BondingScaleFactor = 1.2
@@ -43,7 +44,7 @@ def GetGraph(cstr):
             if distance <= rcut:
                 graph[i,j] = 1
                 graph[j,i] = 1
-
+            
     return graph
 
 def numericalSort(value):
@@ -56,112 +57,64 @@ def numericalSort(value):
     return parts
 
 
-import glob
+
 #Load  Dataset
-reactants_dir = "./KineticPredictorModel/KPM/Data/Test/Aldol/rxn/"
-products_dir = "./KineticPredictorModel/KPM/Data/Test/Aldol/pxn/"
+reactants_dir = "./reactants/"
+products_dir = "./products/"
 # Read Reactant structure files in order
 reactant_files = []
 for file in sorted(glob.glob(reactants_dir+"/react_P*.xyz"),key=numericalSort):
     reactant_files.append(file)
-#print(reactant_files)    
 
 product_files = []
 for file in sorted(glob.glob(products_dir+"/prod_P*.xyz"),key=numericalSort):
     product_files.append(file)
-#print(product_files)    
 
 #Convert all to ASE atoms object
 reactant_struct = []
-for file in reactant_files:   
+for file in reactant_files:
     reactant_struct.append(ase.io.read(file,index=':',format='xyz'))
 
 product_struct = []
-for file in product_files:   
+for file in product_files:
     product_struct.append(ase.io.read(file,index=':',format='xyz'))
 
-# print(product_struct)    
+
 #Define reactant and product frames
-#Select reactant and product frames for benzene dataset    
+
 product = []
 reactant = []
 nreact = len(reactant_files)
 for i in range(nreact):
     product.append(product_struct[i])
     reactant.append(reactant_struct[i])
-#print(product)
 
 reactant = [ r[0] for r in reactant ]
 product = [ r[0] for r in product ]
 
-#Organics- removing non-reactive elements 
-# nreact = len(struct2)
-print(nreact)
 
-gp=[]
-gr=[]
-for i in range(nreact):
-    gr.append(GetGraph(reactant[i]))
-    gp.append(GetGraph(product[i]))
-#print(gr)
-#print(gp)
-##Organics- removing non-reactive elements 
-
-nreact = len(reactant_struct)
+graph = GetGraph(reactant) 
 
 for i in range(nreact):
-    #reactive_atoms=[]
-    n = len(reactant[i].get_chemical_symbols())
-    reactant[i].rxtv = np.zeros(n, dtype='bool')
-    product[i].rxtv = np.zeros(n, dtype='bool')
 
-    reactant[i].rxid = []
-    product[i].rxid = []
-    reactant[i].rxidpair = []
-    graph_P = GetGraph(product[i])
-    graph_R = GetGraph(reactant[i])
+    n = len(reactant2[i].get_chemical_symbols())
+    reactant2[i].rxtv = np.zeros(n, dtype='bool')
+    product2[i].rxtv = np.zeros(n, dtype='bool')
+
+    reactant2[i].rxid = []
+    product2[i].rxid = []
+    reactant2[i].rxidpair = []
+    graph_P = GetGraph(product2[i])
+    graph_R = GetGraph(reactant2[i])
     dG = graph_P - graph_R
     print(dG)
-    '''
-    Here you want to get rid of spectator atoms.
-    i.e. if the atom in reactant i does change in the product 
-    ~ not equal to zero, append, else get rid of.
-    ''' 
     for j in range(n):
         if any(dG[j, :]):
-            reactant[i].rxtv[j] = True
-            reactant[i].rxid.append(j)
-            product[i].rxtv[j] = True
-            product[i].rxid.append(j)
+            reactant2[i].rxtv[j] = True
+            reactant2[i].rxid.append(j)
+            product2[i].rxtv[j] = True
+            product2[i].rxid.append(j)
     for j in range(n - 1):
         for k in range(j + 1, n):
             if dG[j, k] != 0:
-                reactant[i].rxidpair.append([j, k])
-   # Test print of positions for a chosen structure...
-#
-#print(struct[0][0].get_positions())
-print("REACTANT ",reactant[0].get_chemical_symbols())
-print("PRODUCT ",product[0].get_chemical_symbols()) 
-    #reactive_atoms=[]
-#    n = len(reactant[i].get_chemical_symbols())
-#    reactant[i].rxtv = np.zeros(n, dtype='bool')
-#    product[i].rxtv = np.zeros(n, dtype='bool')
-#
-#    reactant[i].rxid = []
-#    product[i].rxid = []
-#    reactant[i].rxidpair = []
-#    graph_P = GetGraph(product[i])
-#    graph_R = GetGraph(reactant[i])
-#    dG = graph_P - graph_R
-#    gp.append(graph_P)
-
-    # for j in range(n):
-    #     if any(dG[j, :]):
-    #         reactant[i].rxtv[j] = True
-    #         reactant[i].rxid.append(j)
-    #         product[i].rxtv[j] = True
-    #         product[i].rxid.append(j)
-    # for j in range(n - 1):
-    #     for k in range(j + 1, n):
-    #         if dG[j, k] != 0:
-    #             reactant[i].rxidpair.append([j, k])
+                reactant2[i].rxidpair.append([j, k])
